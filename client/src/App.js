cb81f71bfd5dd20364b75328342902fe0be18c82
Dan Johnson <danimal.johnson@gmail.com>
2019-11-01 10:55:51 -0700

    Added dark mode and local storage hooks. CSS Styling still required.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import Players from './Players';
 import './App.css';
+import { useDarkMode } from './hooks/useDarkMode';
 
 function App() {
+  const [darkMode, setDarkMode] = useDarkMode(false);
+  const toggleMode = async e => {
+    e.preventDefault();
+    console.log("before", darkMode);
+    await setDarkMode(!darkMode);
+    console.log("after", darkMode);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Womens' World Cup Search Rank</h1>
+        <button onClick={toggleMode}>Toggle Dark</button>
       </header>
       <Players />
     </div>
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -3,8 +3,21 @@ import * as rtl from '@testing-library/react';
 // import ReactDOM from 'react-dom';
 import App from './App';
 
-test('renders without crashing', () => {
+test('Renders without crashing', () => {
   const div = document.createElement('div');
   rtl.render(<App />, div);
   // ReactDOM.unmountComponentAtNode(div);
+});
+
+test('Does simple math', () => {
+  expect(2+2).toBe(4);
+});
+
+test('Header text is correct', () => {
+  const container = rtl.render(<App />);
+  container.findByText("Womens' World Cup Search Rank");
+});
+
+test('Dark button exists', () => {
+
 });
