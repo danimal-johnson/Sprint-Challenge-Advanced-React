e9f75b7c934140bb5d2a691ff50468ae56c37698
Dan Johnson <danimal.johnson@gmail.com>
2019-11-01 10:18:03 -0700

    Test scripts configured. Initial pass

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as rtl from '@testing-library/react';
+// import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
+test('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  rtl.render(<App />, div);
+  // ReactDOM.unmountComponentAtNode(div);
 });
